c10a42b9ed3c1340aae0e2627d5836910e1c7d5c
nando <muhammad.rezki.ananda@gmail.com>
2021-04-24 00:08:28 +0700

    Data Binding

--- app/src/main/java/com/newbiexpert/learndatabindingwithobject/MainActivity.kt
+++ app/src/main/java/com/newbiexpert/learndatabindingwithobject/MainActivity.kt
@@ -2,10 +2,20 @@ package com.newbiexpert.learndatabindingwithobject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.newbiexpert.learndatabindingwithobject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val student = getStudent()
+        binding.nameText.text = student.name
+        binding.emailText.text = student.email
+    }
+
+    private fun getStudent(): Student {
+        return Student(1, "Alex", "alex@gmail.com")
     }
 }
---
+++ app/src/main/java/com/newbiexpert/learndatabindingwithobject/Student.kt
@@ -0,0 +1,7 @@
+package com.newbiexpert.learndatabindingwithobject
+
+data class Student(
+    var id: Int,
+    var name: String,
+    var email: String
+)
